286407978132924e854525ccde76109ffcbf91bb
shuofan18 <stanza@zhangshfandeair.home>
2018-04-02 09:10:11 +1000

    complete code for training
    with 600 training images, acc of test set is now 93%

--- modelling.R
+++ modelling.R
@@ -16,7 +16,7 @@ test_datagen <- image_data_generator(rescale = 1/255)
 train_generator <- flow_images_from_directory(
   train_dir,                                                       
   train_datagen,                                                   
-  target_size = c(300, 300), 
+  target_size = c(150, 150), 
   color_mode = "grayscale",
   batch_size = 20,                                                 
   class_mode = "categorical"
@@ -25,7 +25,7 @@ train_generator <- flow_images_from_directory(
 validation_generator <- flow_images_from_directory(
   validation_dir,
   validation_datagen,
-  target_size = c(300, 300),
+  target_size = c(150, 150),
   color_mode = "grayscale",
   batch_size = 20,
   class_mode = "categorical"
@@ -34,7 +34,7 @@ validation_generator <- flow_images_from_directory(
 test_generator <- flow_images_from_directory(
   test_dir,
   test_datagen,
-  target_size = c(300, 300),
+  target_size = c(150, 150),
   color_mode = "grayscale",
   batch_size = 20,
   class_mode = "categorical"
@@ -44,13 +44,13 @@ test_generator <- flow_images_from_directory(
 # define deep learning model (mnist)
 model <- keras_model_sequential() %>%
   layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
-                input_shape = c(300, 300, 1)) %>%
+                input_shape = c(150, 150, 1)) %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_conv_2d(filters = 256, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_flatten() %>%
   layer_dense(units = 512, activation = "relu") %>%
@@ -66,10 +66,10 @@ model %>% compile(
 
 history <- model %>% fit_generator(
   train_generator,
-  steps_per_epoch = 2,
-  epochs = 5,
+  steps_per_epoch = 100,
+  epochs = 30,
   validation_data = validation_generator,
-  validation_steps = 3
+  validation_steps = 50
 )
 
 ############################################################
