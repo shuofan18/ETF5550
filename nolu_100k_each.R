e5b07ae91ce7508494c1d7ac296b54cadbefd27a
shuofan18 <shuofan.zhang@monash.edu>
2018-05-03 21:43:24 +1000

    combine

--- nolu_100k_each.R
+++ nolu_100k_each.R
@@ -1,5 +1,3 @@
-install.packages("tidyverse")
-
 library(tidyverse)
 library(keras)
 
@@ -81,9 +79,79 @@ mapply(linear, 1:50000)
 setwd("~/p2016030007/szha0004/no_lineup/validation/norela")
 mapply(norela, 1:50000)
 
+######## image generating finish, start training ###########
 
+base_dir <- "~/p2016030007/szha0004/no_lineup"
+train_dir <- file.path(base_dir,"train")
+validation_dir <- file.path(base_dir,"validation")
+test_dir <- file.path(base_dir,"test")
 
+train_datagen <- image_data_generator(rescale = 1/255)             
+validation_datagen <- image_data_generator(rescale = 1/255)  
+test_datagen <- image_data_generator(rescale = 1/255)
 
+train_generator <- flow_images_from_directory(
+  train_dir,                                                       
+  train_datagen,                                                   
+  target_size = c(150, 150), 
+  color_mode = "grayscale",
+  batch_size = 500,                                                 
+  class_mode = "binary"
+)
+
+validation_generator <- flow_images_from_directory(
+  validation_dir,
+  validation_datagen,
+  target_size = c(150, 150),
+  color_mode = "grayscale",
+  batch_size = 200,
+  class_mode = "binary"
+)
+
+test_generator <- flow_images_from_directory(
+  test_dir,
+  test_datagen,
+  target_size = c(150, 150),
+  color_mode = "grayscale",
+  batch_size = 200,
+  class_mode = "binary"
+)
+
+############ define deep learning model ################
+
+model <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+                input_shape = c(150, 150, 1)) %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_flatten() %>%
+  layer_dense(units = 512, activation = "relu") %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+  optimizer = optimizer_rmsprop(lr = 1e-4),
+  loss = "binary_crossentropy",
+  metrics = c("accuracy")
+)
+
+##### changed steps_per_epoch from 100 to 50
+
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = 1000,
+  epochs = 100,
+  validation_data = validation_generator,
+  validation_steps = 500
+)
+
+model %>% evaluate_generator(test_generator, steps = 500)
+
+### how to save the trained model after training? (so it can be used directly in the future)
 
 
 
