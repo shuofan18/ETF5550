e73586cf0090deea5a5a68c9560e12df8d2a7baf
shuofan18 <shuofan.zhang@monash.edu>
2018-05-09 20:55:02 +1000

    5 epoch model

--- results_calculator.R
+++ results_calculator.R
@@ -1,10 +1,21 @@
 
 setwd("/volumes/5550/turk2_results")
 
+# the most common choices by human for each lineup = human_choices
+# the actual location of the real plot in each lineup = newdata$plot_location
+
+
 library(tidyverse)
 data <- read.csv("data_turk2.csv")
+file <- read.csv("/volumes/5550/turk2/file.csv")
 ntrue <- data %>% group_by(pic_name) %>% summarise(ntrue = sum(response))
 count <- table(data$pic_name) %>% data.frame() 
+png_name <- as.character(file$png_nolc)
+human_choices <- vector()
+for (i in 1:70) {
+  datai <- subset(data, pic_name == png_name[i])
+  human_choices[i] <- names(which.max(table(datai$response_no)))
+}
 
 ntrue <- ntrue %>% 
   left_join(select(data, difficulty, pic_name, 
@@ -28,7 +39,7 @@ for (i in 1:70) {
 
 newdata %>% mutate(p_value)
 newdata$conclusion <- (newdata$p_value < 0.05)
-sum(newdata$conclusion)
+acc_human <- sum(newdata$conclusion)/70
 
 difficulty_true <- newdata %>% group_by(difficulty) %>% summarise(pb_true = mean(conclusion))
 
