b221aa792bb677aa02bd710c85808e9d52cd847e
shuofan18 <shuofan.zhang@monash.edu>
2018-05-19 14:04:03 +1000

    heter_train_code

--- heter_experiment.R
+++ heter_experiment.R
@@ -14,12 +14,12 @@ white_test <- function(res, yhat, n){
 }
 ################ function for generate lineup for human experiment #########
 
-heter<-function(i){
+heter_lineup<-function(i){
   
-  n = sample(20:1500, 1)
+  n = sample(100:500, 1)
   x <- runif(n, -1, 1)
   beta <- runif(1,0.5,1)
-  a <- runif(1,0.05,5)*(-1)^(rbinom(1, 1, 0.5))
+  a <- runif(1, 0.05, 5)*(-1)^(rbinom(1, 1, 0.5))
   sd <- a*x+rnorm(n, 0, 1)
   
   min <- min(sd)
@@ -97,37 +97,138 @@ ggsave(filename =
 
 set.seed(0517)
 setwd("/volumes/5550/panda2/lineup")
-accuracy_wtest_heter_train <- sapply(101:200, heter) %>% sum()/100
+accuracy_wtest_heter_train <- sapply(1:10, heter) %>% sum()/10
 accuracy_wtest_heter_train
 
 ############################## generate data for training ######################
 
+heter_train <- function(i){
+  
+  n = sample(100:500, 1)
+  x <- runif(n, -1, 1)
+  beta <- runif(1,0.5,1)
+  a <- runif(1, 0.05, 5)*(-1)^(rbinom(1, 1, 0.5))
+  sd <- a*x+rnorm(n, 0, 1)
+  
+  min <- min(sd)
+  if (min<0){
+    sd <- sd-min
+  }
+  
+  y<-rnorm(n, beta*x, sd)
+  
+  df <- tibble(x, y)
+  model<-lm(y ~ x, data=df)
+  
+  fit <- augment(model, df)
+  wt_stat <- white_test(fit$.resid, fit$.fitted, n)
+  
+  if (wt_stat > crit_value){
+    wt_conclusion <- 1 #1=heter
+  } else {
+    wt_conclusion <- 0 #0=norela
+  }
+#### generate images of "heter"
+  train_data <- fit %>% select(x, .std.resid)
+  ggplot(train_data, aes(x, .std.resid))+geom_point(alpha = 0.4)+
+    theme(axis.line=element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.position="none",
+          panel.background=element_blank(),
+          panel.border=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.background=element_blank(),
+          aspect.ratio = 1)
+  ggsave(filename = 
+           paste(i, "_wtst(", round(wt_stat, digits = 2) ,")_wt(" , wt_conclusion,").png", 
+                 sep = ""), height = 2, width = 2, dpi = 150)
+  return(wt_conclusion)
+}
 
+##########  generate images of linear for training
 
-setwd("/Users/shuofanzhang/documents/monarch/train/norela")
-accuracy_cortest_norela_train_part2 <- (sapply(50001:100000, norela) > 0.05) %>% mean()
-accuracy_cortest_norela_train_part2
+linear_train <- function(i){
+  
+  n = sample(100:500, 1)
+  x <- runif(n, -1, 1)
+  beta <- runif(1,0.5,1)
+  a <- runif(1, 0.05, 5)*(-1)^(rbinom(1, 1, 0.5))
+  sd <- a*x+rnorm(n, 0, 1)
+  min <- min(sd)
+  if (min<0){
+    sd <- sd-min
+  }
+  sd <- mean(sd)
+  y<-rnorm(n, beta*x, sd)
+  
+  df <- tibble(x, y)
+  model<-lm(y ~ x, data=df)
+  
+  fit <- augment(model, df)
+  wt_stat <- white_test(fit$.resid, fit$.fitted, n)
+  
+  if (wt_stat > crit_value){
+    wt_conclusion <- 1 #1=heter
+  } else {
+    wt_conclusion <- 0 #0=norela
+  }
+  #### generate images of "linear"
+  train_data <- fit %>% select(x, .std.resid)
+  ggplot(train_data, aes(x, .std.resid))+geom_point(alpha = 0.4)+
+    theme(axis.line=element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.position="none",
+          panel.background=element_blank(),
+          panel.border=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.background=element_blank(),
+          aspect.ratio = 1)
+  ggsave(filename = 
+           paste(i, "_wtst(", round(wt_stat, digits = 2) ,")_wt(" , wt_conclusion,").png", 
+                 sep = ""), height = 2, width = 2, dpi = 150)
+  return(wt_conclusion)
+}
 
-setwd("/Users/shuofanzhang/documents/monarch/test/linear")
-accuracy_cortest_linear_test_part2 <- (sapply(20001:40000, linear) < 0.05) %>% mean()
-accuracy_cortest_linear_test_part2
-setwd("/Users/shuofanzhang/documents/monarch/test/norela")
-accuracy_cortest_norela_test_part2 <- (sapply(20001:40000, norela) > 0.05) %>% mean()
-accuracy_cortest_norela_test_part2
-
-setwd("/Users/shuofanzhang/documents/monarch/validation/linear")
-accuracy_cortest_linear_validation_part2 <- (sapply(20001:40000, linear) < 0.05) %>% mean()
-accuracy_cortest_linear_validation_part2 
-setwd("/Users/shuofanzhang/documents/monarch/validation/norela")
-accuracy_cortest_norela_validation_part2 <- (sapply(20001:40000, norela) > 0.05) %>% mean()
-accuracy_cortest_norela_validation_part2
-
+###### save heter images to train folder
+setwd("/Users/shuofanzhang/documents/heter&linear/train/heter")
+accuracy_wtest_heter_train <- sapply(1:100000, heter_train) %>% mean()
+accuracy_wtest_heter_train
+###### save heter images to test folder
+setwd("/Users/shuofanzhang/documents/heter&linear/test/heter")
+accuracy_wtest_heter_test <- sapply(1:40000, heter_train) %>% mean()
+accuracy_wtest_heter_test
+###### save heter images to validation folder
+setwd("/Users/shuofanzhang/documents/heter&linear/validation/heter")
+accuracy_wtest_heter_validation <- sapply(1:40000, heter_train) %>% mean()
+accuracy_wtest_heter_validation
+###### save linear images to train folder
+setwd("/Users/shuofanzhang/documents/heter&linear/train/linear")
+accuracy_wtest_linear_train <- sapply(1:100000, linear_train) %>% mean()
+accuracy_wtest_linear_train
+###### save linear images to test folder
+setwd("/Users/shuofanzhang/documents/heter&linear/test/heter")
+accuracy_wtest_linear_test <- sapply(1:40000, linear_train) %>% mean()
+accuracy_wtest_linear_test
+###### save linear images to validation folder
+setwd("/Users/shuofanzhang/documents/heter&linear/validation/heter")
+accuracy_wtest_linear_validation <- sapply(1:40000, linear_train) %>% mean()
+accuracy_wtest_linear_validation
 
 ######## image generating finish, start training ########### need to start from here ################## 8th May 2018 afternoon 
 
-setwd("/Volumes/5550/panda")
+setwd("/Users/shuofanzhang/documents/heter&linear")
 
-base_dir <- "/Volumes/5550/panda"
+base_dir <- "/Users/shuofanzhang/documents/heter&linear"
 train_dir <- file.path(base_dir,"train")
 validation_dir <- file.path(base_dir,"validation")
 test_dir <- file.path(base_dir,"test")
@@ -136,11 +237,9 @@ train_datagen <- image_data_generator(rescale = 1/255)
 validation_datagen <- image_data_generator(rescale = 1/255)  
 test_datagen <- image_data_generator(rescale = 1/255)
 
-
-
 ############ define deep learning model ################
 
-model <- keras_model_sequential() %>%
+model_heter <- keras_model_sequential() %>%
   layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
                 input_shape = c(150, 150, 1)) %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
@@ -189,38 +288,7 @@ test_generator <- flow_images_from_directory(
   class_mode = "binary"
 )
 
-history <- model %>% fit_generator(
-  train_generator,
-  steps_per_epoch = 2000,
-  epochs = 5,
-  callbacks = callback_model_checkpoint("weights.{epoch:02d}-{val_loss:.2f}.hdf5"),
-  validation_data = validation_generator,
-  validation_steps = 800
-)
-
-plot(history)
-##################################################another model fitting try###########
-setwd("/Users/shuofanzhang/documents/monarch")
-model59 <- keras_model_sequential() %>%
-  layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
-                input_shape = c(150, 150, 1)) %>%
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  layer_flatten() %>%
-  layer_dense(units = 512, activation = "relu") %>%
-  layer_dense(units = 1, activation = "sigmoid")
-
-model59 %>% compile(
-  optimizer = optimizer_rmsprop(lr = 1e-4),
-  loss = "binary_crossentropy",
-  metrics = c("accuracy")
-)
-history59 <- model59 %>% fit_generator(
+history_heter <- model_heter %>% fit_generator(
   train_generator,
   steps_per_epoch = 2000,
   epochs = 10,
@@ -229,12 +297,12 @@ history59 <- model59 %>% fit_generator(
   validation_steps = 800
 )
 
-model59 %>% save_model_hdf5("new_100k_each_59.h5")
+plot(history_heter)
 
 ################################################# evaluate convnets on test set##############################
 dl_acc_test_hat <- model %>% evaluate_generator(test_generator, steps = 800)
 
-################# evaluate convnets on turk experiment data (70 real plots only) ###### human=0.5714286
+################# evaluate convnets on new experiment data (29 real plots only) ###### human=0.5714286
 
 evaluate_turk70real <- flow_images_from_directory(
   "/Users/shuofanzhang/documents/monarch/turk70real",
