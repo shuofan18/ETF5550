01464797d5875aeb7885b323ecce39694b2461b1
shuofan18 <shuofan.zhang@monash.edu>
2018-05-20 21:00:44 +1000

    update

--- t-test-4-fun.R
+++ t-test-4-fun.R
@@ -24,10 +24,24 @@ norela<-function(i){
   return(ct$p.value)
 }
 
-acc_t_power <- (sapply(1:200000, linear) < 0.05) %>% mean()
-acc_t_power
-acc_t_1_alpha <- (sapply(1:200000, norela) > 0.05) %>% mean()
-acc_t_1_alpha
+acc_calculator <- function(alpha, i){
+  alpha <- alpha*0.005
+  power <- (sapply(1:i, linear) < alpha) %>% mean()
+  alpha_acc <- (sapply(1:i, norela) > alpha) %>% mean()
+  acc <- (power+alpha_acc)/2
+  res <- c(alpha, acc)
+  return(res)
+}
+
+results <- sapply(1:20, acc_calculator, i=200000)
+results <- results %>% as.data.frame() %>% t()
+results[,2] %>% which.max()  ###### when alpha=0.02, acc is maximized
+
+
+#acc_t_power <- (sapply(1:200000, linear) < 0.05) %>% mean()
+#acc_t_power
+#acc_t_1_alpha <- (sapply(1:200000, norela) > 0.05) %>% mean()
+#acc_t_1_alpha
 
 
 
