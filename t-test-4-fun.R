3811633638b18336dcff0ab707216f6f05443418
shuofan18 <shuofan.zhang@monash.edu>
2018-05-25 16:36:58 +1000

    update

--- t-test-4-fun.R
+++ t-test-4-fun.R
@@ -29,7 +29,7 @@ acc_calculator <- function(alpha, i){
   power <- (sapply(1:i, linear) < alpha) %>% mean()
   alpha_acc <- (sapply(1:i, norela) > alpha) %>% mean()
   acc <- (power+alpha_acc)/2
-  res <- c(alpha, acc)
+  res <- c(alpha, acc, power, alpha_acc)
   return(res)
 }
 
@@ -42,6 +42,29 @@ results[,2] %>% which.max()  ###### when alpha=0.02, acc is maximized
 #acc_t_power
 #acc_t_1_alpha <- (sapply(1:200000, norela) > 0.05) %>% mean()
 #acc_t_1_alpha
+
+white_power_test <- read.csv("/volumes/5550/heter&linear/parameters/parameters6.csv")
+white_power_test <- white_power_test$wt_conclusion %>% mean()
+library(keras)
+model_loaded_15 <- load_model_hdf5("/Volumes/5550/heter&linear/checkpoints/523new15epoches/weights.11-0.08.hdf5") 
+base_dir <- "/Volumes/5550/heter&linear"
+test_dir <- file.path(base_dir,"test")
+test_datagen <- image_data_generator(rescale = 1/255)
+test_generator <- flow_images_from_directory(
+  test_dir,
+  test_datagen,
+  target_size = c(150, 150),
+  color_mode = "grayscale",
+  batch_size = 100,
+  class_mode = "binary"
+)
+  
+acc11epoch_power <- model_loaded_15 %>% evaluate_generator(test_generator, steps = 1000)
+
+model_loaded_4 <- load_model_hdf5("/Volumes/5550/heter&linear/checkpoints/523new15epoches/weights.04-0.09.hdf5")
+acc4epoch_power <- model_loaded_4 %>% evaluate_generator(test_generator, steps = 1000)
+
+
 
 
 
