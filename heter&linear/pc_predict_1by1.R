d4e9b82e73b33d0fe57f6c282bac7aea982745e0
shuofan18 <shuofan.zhang@monash.edu>
2018-05-27 15:03:05 +1000

    update

--- heter&linear/pc_predict_1by1.R
+++ heter&linear/pc_predict_1by1.R
@@ -15,19 +15,29 @@ lineup_test <- flow_images_from_directory(
 prediction <- heter11model %>% predict_generator(lineup_test, steps = 1)
 prediction 
 
-################## make graph 
+################## make graph of pvalues from human, computer, and white test
 library(reshape2)
 survey <- read.csv("~/documents/github/etf5550/heter&linear/survey_ans_nonull.csv")
 survey <- survey %>% mutate(wtpvalue=1-pchisq(wt.stat,2))
 graphdata <- survey %>% select(form, question, pval, pcpvalue, wtpvalue, a) %>% 
   mutate(id=1:27)
-graphdata <- graphdata %>% melt(id=c("form","question","id"))
-#graphdata <- graphdata %>% filter(variable!="pval")
-ggplot(graphdata, aes(x=id, y=value, group=variable, color=variable))+
+colnames(graphdata) <- c("form", "question", "Human p.values",
+                         "Computer prediction of Null", "White-test p.values",
+                         "a", "ID")
+
+graphdata <- graphdata %>% melt(id=c("form","question","ID","a"))
+
+ggplot(graphdata, aes(x=ID, y=value, group=variable, color=variable))+
   geom_point()+
   geom_line()+
-  facet_wrap(~variable, nrow = 3)
-
+  facet_wrap(~variable, nrow = 3)+
+  xlab("Lineup Questions")
+ # scale_x_discrete(name="Lineup Questions", breaks=1:27, 
+  #                 labels=c("1","2","3","4","5","6","7","8",
+   #                         "9","10","11","12","13","14","15",
+    #                        "16","17","18","19","20","21","22",
+     #                       "23","24","25","26","27"))
+#ggsave(filename = "~/documents/github/etf5550/heter&linear/graph_pvalues_3tests.png")
 
 
 
